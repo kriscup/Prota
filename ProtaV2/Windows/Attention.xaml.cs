3a55ad934677682dbeae2dd4de2e4180241c87a3
Jacob Fisher <jfisher_6173@email.ric.edu>
2022-11-08 17:22:46 -0500

    reset stuff

---
+++ ProtaV2/Windows/Attention.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProtaV2.Windows {
+    /// <summary>
+    /// Interaction logic for Attention.xaml
+    /// </summary>
+    public partial class Attention : Window {
+        public Attention() {
+            InitializeComponent();
+        }
+
+        private void REconfirmYes_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void REconfirmNo_Click(object sender, RoutedEventArgs e) {
+            Close();
+        }
+    }
+}
